3c807115e00001991e1164f7e67b186c894efe41
s094392 <s094392@gmail.com>
2015-12-20 11:41:29 +0800

    add socket

---
+++ README.md
@@ -0,0 +1,18 @@
+OXX
+####started from 2015.12.18
+
+##說明
+    使用node.js 搭配socket.io, express製作的ooxx線上對戰小遊戲
+    當作node.js的練習
+
+##運作方法
+```bash
+npm install
+node index.js
+```
+
+##套件
+    express
+    material-design-lite
+    socket.io
+
--- index.js
+++ index.js
@@ -2,10 +2,15 @@ var app = require('express')();
 var express = require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-
+var online = 0;
 app.use('/node_modules', express.static(__dirname + '/node_modules'));
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+    online ++;
+    console.log(online + ' online');
 });
 
 http.listen(3000, function(){
